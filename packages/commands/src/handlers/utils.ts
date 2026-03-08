2ad81f71e544a81193c32823b522815e91e38f8b
Angel Pichardo <pichardoangel416@gmail.com>
2026-03-08 13:59:37 -0400

    update the JSDoc to better reflect the implementation

--- packages/commands/src/handlers/utils.ts
+++ packages/commands/src/handlers/utils.ts
@@ -2,14 +2,14 @@ import { getItem, type ItemAPIParams } from "@arctools/arc-data";
 import { normalize, slugify } from "@arctools/utils";
 import { Effect, Option } from "effect";
 
-/** Attempts to resolve item by id first, if fails, resolves by search query */
+/** Attempts to resolve item by id first, if none found, resolves by search query */
 export const resolveItem = Effect.fnUntraced(function* (
   search: string,
   params: Omit<ItemAPIParams, "id" | "search">,
 ) {
   return yield* Effect.filterOrElse(
-    getItem({ id: slugify(search), ...params }),
+    getItem({ ...params, id: slugify(search) }),
     (result) => Option.isSome(result),
-    () => getItem({ search: normalize(search), ...params }),
+    () => getItem({ ...params, search: normalize(search) }),
   );
 });
